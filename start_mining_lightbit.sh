5c09daf8427d6ec85d803291c20366540f8523cb
sagemantap <158444606+sagemantap@users.noreply.github.com>
2025-06-21 17:15:34 +0700

    Update start_mining_lightbit.sh

--- start_mining_lightbit.sh
+++ start_mining_lightbit.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while [ 1 ]; do
-./cpuminer -o stratum+tcp://cpu-pool.com:63398 -u WALLET_ADDRESS --algo yespower --param-n 2048 --param-r 32 --param-key "LITBpower: The number of LITB working or available for proof-of-work mini"
+./cpuminer -o stratum+tcp://cpustratum-asia.rplant.xyz:7022 -u mbc1qdhlx63rgvqygutsyhyf40r4ayyfuykurj7t8ae --algo power2b --param-n 2048 --param-r 32 --param-key "LITBpower: The number of LITB working or available for proof-of-work mini"
 sleep 10
 done
