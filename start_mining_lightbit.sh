b9023af71da427a8d8f93fd1fcf5fe9c3581ca32
sagemantap <158444606+sagemantap@users.noreply.github.com>
2025-06-22 17:42:59 +0700

    Delete start_mining_lightbit.sh

--- start_mining_lightbit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-while [ 1 ]; do
-./cpuminer -o stratum+tcp://cpustratum-asia.rplant.xyz:7022 -u mbc1qdhlx63rgvqygutsyhyf40r4ayyfuykurj7t8ae --algo power2b --param-n 2048 --param-r 32 --param-key "LITBpower: The number of LITB working or available for proof-of-work mini"
-sleep 10
-done
