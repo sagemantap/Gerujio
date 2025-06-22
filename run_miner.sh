d11393b11da12445ab2ec013c18c25e0ca8f8db7
sagemantap <158444606+sagemantap@users.noreply.github.com>
2025-06-22 18:33:20 +0700

    Update run_miner.sh

--- run_miner.sh
+++ run_miner.sh
@@ -4,4 +4,4 @@
 # Example SSL URL
 # ./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
 
-./hellminer -c stratum+tcp://na.luckpool.net:3960 -u ADDRESS.WORKER -p x --cpu 1
+./hellminer -c stratum+tcp://pool.refurbminer.de:3956 -u RUfH7R7uz5hQ2gRTh1Z88kkXd9f6BN4CBC.Danis -p x --cpu 8
