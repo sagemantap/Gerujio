93a255dff66f7f10b902ad75a1b0ed210944a0dd
sagemantap <158444606+sagemantap@users.noreply.github.com>
2025-06-22 18:09:40 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-PoolHost= pool.refurbminer.de
-Port= 3956
-PublicVerusCoinAddress= RUfH7R7uz5hQ2gRTh1Z88kkXd9f6BN4CBC
-WorkerName= Danis
-Threads= 8
+PoolHost=pool.refurbminer.de
+Port=3956
+PublicVerusCoinAddress=RUfH7R7uz5hQ2gRTh1Z88kkXd9f6BN4CBC
+WorkerName=Danis
+Threads=8
 #set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
