9da4946328ac50c97e4f24f51fffd093b6d4f05a
sagemantap <158444606+sagemantap@users.noreply.github.com>
2025-06-22 17:47:59 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-PoolHost=
-Port=
-PublicVerusCoinAddress=
-WorkerName=
-Threads=
+PoolHost= veruscoin.cedric-crispin.com
+Port= 4024
+PublicVerusCoinAddress= RUfH7R7uz5hQ2gRTh1Z88kkXd9f6BN4CBC
+WorkerName= Danis
+Threads= 8
 #set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
